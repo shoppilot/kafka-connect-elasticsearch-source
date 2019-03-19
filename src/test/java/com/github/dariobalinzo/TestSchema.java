9f58987a8b7f30bd5f0863884a2d9205382732fc
gialuan@inspectorio.com <gialuan@inspectorio.com>
2019-03-19 17:40:49 +0700

    update jar package

--- src/test/java/com/github/dariobalinzo/TestSchema.java
+++ src/test/java/com/github/dariobalinzo/TestSchema.java
@@ -34,10 +34,8 @@ import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class TestSchema extends TestCase {
 
